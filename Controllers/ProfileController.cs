3acd77cc245263e818c0b8d46e55368c82171cd4
Mahekaru <Mahekaru@gmail.com>
2021-04-06 15:48:43 -0400

    Fixed Title Simplified controller added Skills

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -12,7 +12,12 @@ namespace MahekaruProfileCore.Controllers
         public IActionResult Index()
         {
             ProfileModel vm = new ProfileModel();
-            
+            vm = FillViewModel(vm);
+            return View(vm);
+        }
+
+        private ProfileModel FillViewModel(ProfileModel vm)
+        {
             vm.Name = "Michael Jennings";
             vm.Exp = 173394;
             vm.ExpTotal = 4470000;
@@ -25,7 +30,7 @@ namespace MahekaruProfileCore.Controllers
             vm.ProfileImage = "/img/profile/ProfileImage.png";
 
             vm.Skills.Add(AddSkill("React", 1.5));
-            vm.Skills.Add(AddSkill("Node.js", 4.3));
+            vm.Skills.Add(AddSkill("Node.js", 5.3));
             vm.Skills.Add(AddSkill("Angular", 5.8));
             vm.Skills.Add(AddSkill("MEAN Stack", 6));
             vm.Skills.Add(AddSkill("OOP", 8));
@@ -37,42 +42,12 @@ namespace MahekaruProfileCore.Controllers
             vm.Skills.Add(AddSkill("Automation", 8));
             vm.Skills.Add(AddSkill("Application Development", 10));
             vm.Skills.Add(AddSkill("Visual Studio", 10));
+            vm.Skills.Add(AddSkill("MVVM", 6));
+            vm.Skills.Add(AddSkill("Aurelia", 6));
 
-            return View(vm);
+            return vm;
         }
 
-        public IActionResult Test()
-        {
-            ProfileModel vm = new ProfileModel();
-
-            vm.Name = "Michael Jennings";
-            vm.Exp = 173394;
-            vm.ExpTotal = 4470000;
-            vm.HP = 33041;
-            vm.MP = 17650;
-            vm.LinkedIn = "";
-            vm.Email = "MichaelEJennings@yahoo.com";
-            vm.Title = "LEVEL 73 Programmer";
-            vm.GitHub = "https://github.com/MichaelEJennings";
-            vm.ProfileImage = "/img/profile/ProfileImage.png";
-
-            vm.Skills.Add(AddSkill("React", 1.5));
-            vm.Skills.Add(AddSkill("Node.js", 4.3));
-            vm.Skills.Add(AddSkill("Angular", 5.8));
-            vm.Skills.Add(AddSkill("MEAN Stack", 6));
-            vm.Skills.Add(AddSkill("OOP", 8));
-            vm.Skills.Add(AddSkill("SQL", 8.4));
-            vm.Skills.Add(AddSkill("C#", 9));
-            vm.Skills.Add(AddSkill("Javascript", 8));
-            vm.Skills.Add(AddSkill("CSS", 7));
-            vm.Skills.Add(AddSkill("VB", 7.5));
-            vm.Skills.Add(AddSkill("Automation", 8));
-            vm.Skills.Add(AddSkill("Application Development", 10));
-            vm.Skills.Add(AddSkill("Visual Studio", 10));
-            vm.Skills.OrderByDescending(x => x.SkillPoints);
-            //--vm.Skills.OrderBy(x => x.SkillPoints);
-            return View(vm);
-        }
         private Skills AddSkill(string name, double skillPoints)
         {
             Skills skill = new Skills();
